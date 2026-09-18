a7894b230004a9fbddb839c236a3adeb75e9902b
antoinealliaume <antoinealliaume12@gmail.com>
2026-09-18 17:55:12 +0200

    feat(publication): attach durable publication API

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .worker_status import attach as _attach_worker_status
 from .local_worker_api2 import attach as _attach_local_worker_api
 from .media_api import attach as _attach_media_api
 from .storage_api import attach as _attach_storage_api
+from .publication_api import attach as _attach_publication_api
 from .security import attach as _attach_security
 
 _BaseFastAPI = _fastapi.FastAPI
@@ -22,6 +23,7 @@ class AutoDirectorFastAPI(_BaseFastAPI):
         _attach_local_worker_api(self)
         _attach_media_api(self)
         _attach_storage_api(self)
+        _attach_publication_api(self)
 
 
 _fastapi.FastAPI = AutoDirectorFastAPI
